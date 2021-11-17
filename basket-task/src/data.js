419fa4667c2912b0c7ec5bb805802086a26a7afe
YanSido <yansido1@gmail.com>
2021-11-18 01:46:42 +0200

    Added products section

---
+++ basket-task/src/data.js
@@ -0,0 +1,29 @@
+import img1 from "./img/img1.jpg"
+import img2 from "./img/img2.jpg"
+import img3 from "./img/img3.jpg"
+
+
+const data = {
+    productData:[
+        {
+            id: 1,
+            img: img1,
+            title: "Banana",
+            desc: "",
+        },
+        {
+            id: 2,
+            img: img2,
+            title: "Strawberry",
+            desc: "",
+        },
+        {
+            id: 3,
+            img: img3,
+            title: "Apple",
+            desc: "",
+        },
+    ],
+}
+
+export default data;
